de188d8c260b5b393c641e812def5c5849df6216
Hoan <Hoan.kma2x@gmail.com>
2023-05-25 12:12:46 +0700

    sửa phần làm lại đề thi

--- src/exam-history/exam-history.service.ts
+++ src/exam-history/exam-history.service.ts
@@ -27,8 +27,8 @@ export class ExamHistoryService {
     private readonly subjectService: SubjectService,
   ) {}
 
-  async findAll(query: Record<string, unknown>): Promise<any> {
-    const { page, size, keyword = '', studentId } = query || {};
+  async findAll(query: any): Promise<any> {
+    const { page, size, keyword = '', studentId, sort } = query || {};
 
     const pageQuery = Number(page) || 1;
     const sizeQuery = Number(size) || 10;
@@ -36,10 +36,21 @@ export class ExamHistoryService {
       { status: 'ACTIVE', title: { $regex: '.*' + keyword + '.*' }, studentId },
       identity,
     );
+    const querySort = {};
+    if (sort) {
+      const [field, type] = sort.split(' ');
+      if (type === 'asc') {
+        querySort[field] = 1;
+      }
+      if (type === 'desc') {
+        querySort[field] = -1;
+      }
+    }
     const numOfItem = await this.model.count(queryDb);
 
     const dataList = await this.model
       .find(queryDb)
+      .sort(querySort)
       .limit(sizeQuery)
       .skip(pageQuery > 1 ? (pageQuery - 1) * sizeQuery : 0);
 
@@ -144,8 +155,8 @@ export class ExamHistoryService {
     }
 
     const { examId } = createExamHistoryDto;
-    let exam:any = null
-    if(examId){
+    let exam: any = null;
+    if (examId) {
       exam = await this.examService.findOne(examId);
     }
     const title = examId ? exam.title : 'Đề thi ngẫu nhiên';
@@ -158,7 +169,7 @@ export class ExamHistoryService {
       status: 'ACTIVE',
     }).save();
 
-    if(exam){
+    if (examId && examId) {
       await this.examService.update(
         examId,
         {
